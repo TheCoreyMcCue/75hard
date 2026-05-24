59ea228af6ad7727d5ffa96e15668472e16e9e28
thecoreymccue <corey.mccue@datadoghq.com>
2026-05-24 21:44:54 +0200

    fix: remove preconnect link tags causing hydration mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <body className="min-h-full">
-        <link rel="preconnect" href="https://browser-intake-datadoghq.com" crossOrigin="" />
-        <link rel="preconnect" href="https://session-replay-datadoghq.com" crossOrigin="" />
-        <link rel="dns-prefetch" href="https://browser-intake-datadoghq.com" />
         <DatadogInit />
         {children}
       </body>
